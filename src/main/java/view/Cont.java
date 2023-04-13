73303574058cacadbd8f0e8493abb70b21f69cfe
Christopher (Preston) Dickerson <cdickerson39@yahoo.com>
2023-04-13 15:29:14 -0400

    Got it working on multiple

--- src/main/java/view/Cont.java
+++ src/main/java/view/Cont.java
@@ -1,41 +1,63 @@
 package view;
 
-
 import java.io.IOException;
-
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
+import javafx.scene.layout.BorderPane;
+import javafx.stage.Stage;
 import model.ViewTransitionalModel;
 import javafx.beans.binding.Bindings;
 
 public class Cont {
-	ViewTransitionalModel model;
-	
-	public void setModel(ViewTransitionalModel newmodel) {
-		model=newmodel;
-	}
-	
-	@FXML
-	private Button signin;
-	@FXML
-	private TextField username;
-	@FXML
-	private TextField pw;
-	
-	@FXML
-	void onsignin(ActionEvent event) throws IOException {
-		model.showgrouppage();
-	}
-	
-	@FXML
-	void onFortnite(ActionEvent event) throws IOException {
-		model.showfortnitegroupchats();
-	}
-	
-	@FXML
-	void onMinecraft(ActionEvent event) throws IOException {
-		model.showminecraft();
-	}
+    private ViewTransitionalModel model;
+
+    public void setModel(ViewTransitionalModel model) {
+        this.model = model;
+    }
+
+    @FXML
+    void onsignin(ActionEvent event) throws IOException {
+        FXMLLoader loader = new FXMLLoader();
+        loader.setLocation(getClass().getResource("../view/grouppage.fxml"));
+        BorderPane view = loader.load();
+        model = new ViewTransitionalModel(view);
+        Cont controller = loader.getController();
+        controller.setModel(model);
+        Scene scene = new Scene(view);
+        Stage stage = (Stage) ((Button) event.getSource()).getScene().getWindow();
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    @FXML
+    void onFortnite(ActionEvent event) throws IOException {
+        FXMLLoader loader = new FXMLLoader();
+        loader.setLocation(getClass().getResource("../view/Fortnitegroupchats.fxml"));
+        BorderPane view = loader.load();
+        model = new ViewTransitionalModel(view);
+        Cont controller = loader.getController();
+        controller.setModel(model);
+        Scene scene = new Scene(view);
+        Stage stage = (Stage) ((Button) event.getSource()).getScene().getWindow();
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    @FXML
+    void onMinecraft(ActionEvent event) throws IOException {
+        FXMLLoader loader = new FXMLLoader();
+        loader.setLocation(getClass().getResource("../view/Minecraftgroupchats.fxml"));
+        BorderPane view = loader.load();
+        model = new ViewTransitionalModel(view);
+        Cont controller = loader.getController();
+        controller.setModel(model);
+        Scene scene = new Scene(view);
+        Stage stage = (Stage) ((Button) event.getSource()).getScene().getWindow();
+        stage.setScene(scene);
+        stage.show();
+    }
 }
